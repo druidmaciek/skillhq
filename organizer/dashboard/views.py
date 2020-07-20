319c29bfa31df3de4250a5f3d3090e4ae4fc98b5
druidmaciek <maciekjanowski42@icloud.com>
2020-07-20 10:48:39 +0200

    add @login_required to dashboard home, and pass logged in user's resources in context

--- organizer/dashboard/views.py
+++ organizer/dashboard/views.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Resource
 
+
+@login_required
 def dashboard(request):
+    resources = Resource.objects.filter(user=request.user)
     return render(request, 'dashboard/dashboard.html',
-                  {'section': 'dashboard'})
+                  {'section': 'dashboard',
+                   'resources': resources})
