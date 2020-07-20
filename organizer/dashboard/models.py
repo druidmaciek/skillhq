cb65d1fa4e0294dceb4ec2e40a41722c27226151
druidmaciek <maciekjanowski42@icloud.com>
2020-07-20 10:42:34 +0200

    finish slide-over design, create models for Resources and Notes, create forms.py file

--- organizer/dashboard/models.py
+++ organizer/dashboard/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Resource(models.Model):
+    STATUS_CHOICES = (
+        ('completed', 'Completed'),
+        ('in_progress', 'In Progress'),
+        ('paused', "Not Started/On Hold")
+    )
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='resources')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=30,
+                              choices=STATUS_CHOICES,
+                              default='paused')
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+
+class Note(models.Model):
+    resource = models.ForeignKey(Resource,
+                                 on_delete=models.CASCADE,
+                                 related_name='notes')
+    content = models.TextField()
