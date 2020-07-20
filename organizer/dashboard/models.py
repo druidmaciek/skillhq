7855ae796ca508880e116c97fea476b1b8d02625
druidmaciek <maciekjanowski42@icloud.com>
2020-07-20 10:54:00 +0200

    add resource_type field to Resource model

--- organizer/dashboard/models.py
+++ organizer/dashboard/models.py
@@ -8,14 +8,26 @@ class Resource(models.Model):
         ('in_progress', 'In Progress'),
         ('paused', "Not Started/On Hold")
     )
+    TYPE_CHOICES = (
+        ('video_course', "Video Course"),
+        ('text_course', 'Text Course'),
+        ('text_tutorial', 'Text Tutorial'),
+        ('video_tutorial', 'Video Tutorial'),
+        ('ebook', 'eBook'),
+        ('book', 'Book'),
+        ('other', 'Other'),
+    )
     title = models.CharField(max_length=100)
     subject = models.CharField(max_length=100)
+    resource_type = models.CharField(max_length=100,
+                                     choices=TYPE_CHOICES,
+                                     default='other')
     resource_url = models.URLField(max_length=255,
                                    null=True,
                                    default=None)
     user = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='resources')
+                             on_delete=models.CASCADE,
+                             related_name='resources')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=30,
