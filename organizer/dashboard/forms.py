c15cd7bad6c31d4c7016db3f1619570a13a17375
druidmaciek <maciekjanowski42@icloud.com>
2020-07-20 11:15:52 +0200

    created form for adding resources and implemented it in dashboard view

--- organizer/dashboard/forms.py
+++ organizer/dashboard/forms.py
@@ -1,2 +1,10 @@
+from django import forms
+
+from .models import Resource
 
 
+class AddResourceForm(forms.ModelForm):
+    class Meta:
+        model = Resource
+        fields = ("title", "subject", "resource_type",
+                  "url")
--- organizer/dashboard/views.py
+++ organizer/dashboard/views.py
@@ -1,12 +1,26 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .forms import AddResourceForm
 from .models import Resource
 
 
 @login_required
 def dashboard(request):
+
+    # TODO implement in front-end and delete this
+    # TEMPORARY, ultimately it should be called by Vue.js axios in front end
+    if request.method == "POST":
+        form = AddResourceForm(request.POST)
+        if form.is_valid():
+            resource = form.save(commit=False)
+            resource.user = request.user
+            resource.save()
+    else:
+        form = AddResourceForm()
+
     resources = Resource.objects.filter(user=request.user)
     return render(request, 'dashboard/dashboard.html',
                   {'section': 'dashboard',
-                   'resources': resources})
+                   'resources': resources,
+                   'form': form})
