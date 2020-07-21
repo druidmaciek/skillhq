010c7bd20bff9b8a26fe5cf8b228640135589560
druidmaciek <maciekjanowski42@icloud.com>
2020-07-21 12:00:01 +0200

    created serializers for Task and Resource

--- organizer/dashboard/api/serializers.py
+++ organizer/dashboard/api/serializers.py
@@ -1,3 +1,18 @@
 from rest_framework import serializers
 
-from ..models import Resource,
+from ..models import Resource, Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('title', 'status')
+
+
+class ResourceSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(required=False)
+
+    class Meta:
+        model = Resource
+        fields = ('title', 'subject', 'resource_type', 'url',
+                  'status')
