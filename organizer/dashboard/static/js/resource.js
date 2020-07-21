02de76d84880ae4665c4049810dda56635e74d5a
druidmaciek <maciekjanowski42@icloud.com>
2020-07-21 22:13:21 +0200

    add delete resource functionality

---
+++ organizer/dashboard/static/js/resource.js
@@ -0,0 +1,19 @@
+
+
+// Delete Resource
+
+const deleteResource = async (resourceId) => {
+    const csrftoken = document.querySelector('#details-form > input').value;
+    try {
+    const res = await axios.delete('/api/resources/' + resourceId + '/',
+        { headers: { 'X-CSRFToken': csrftoken }}
+        );
+    const deleted = res.data;
+    console.log(`Resource deleted!`, deleted);
+    // TODO replace refresh with adding dynamically
+    location.href = '/';
+    return deleted;
+      } catch (e) {
+        console.error(e);
+      }
+};
