1e490068893d533186e54ad66d455d9fdf3050e4
druidmaciek <maciekjanowski42@icloud.com>
2020-07-21 11:54:30 +0200

    Add restframework, and create api package in dashboard app

--- organizer/organizer/settings.py
+++ organizer/organizer/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'rest_framework',
 
     'dashboard',
 ]
@@ -135,3 +135,10 @@ LOGOUT_URL = "account:logout"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.djangoModelPermissionsOrAnonReadOnly'
+    ]
+}
