60f7e7198d25726e21c6fe46162213d18832bdd1
druidmaciek <maciekjanowski42@icloud.com>
2020-07-22 19:52:41 +0200

    set up pytest, and ping route for test

--- organizer/organizer/urls.py
+++ organizer/organizer/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls', namespace='account')),
     path('', include('dashboard.urls', namespace='dashboard')),
-    path('api/', include('dashboard.api.urls', namespace='api'))
+    path('api/', include('dashboard.api.urls', namespace='api')),
+    path('ping/', views.ping, name="ping"),
 ]
