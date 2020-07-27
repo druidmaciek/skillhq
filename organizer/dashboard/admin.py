d5cdf71031f2991f4c07e71c3e3050ebe91fbd30
druidmaciek <maciekjanowski42@icloud.com>
2020-07-27 09:35:20 +0200

    add subject model to admin panel

--- organizer/dashboard/admin.py
+++ organizer/dashboard/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Note, Resource
+from .models import Note, Resource, Task, Goal, Post, Subject, Comment
+
 
 admin.site.register(Resource)
 admin.site.register(Note)
+admin.site.register(Subject)
+admin.site.register(Post)
+admin.site.register(Comment)
